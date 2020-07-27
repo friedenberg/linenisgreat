cd3fae314e8aae62ca9dafbeb3250621facb5310
Sasha Friedenberg <sashafriedenberg@gmail.com>
2020-07-27 14:48:33 -0400

    use AND'd query for selector-based search

--- public/javascript.js
+++ public/javascript.js
@@ -3,14 +3,21 @@ function updateResults() {
   let stylesheet = document.styleSheets[1];
   let searchBox = document.getElementById("search-box");
 
-  let query = searchBox.value;
-  let rule = `#search-box[value=${query}] ~ .card-grid > .card:not([data-match*=${query}]) { display: none; }`;
+  let value = searchBox.value;
+  var rule = null;
 
-  for (let i = 0; i < stylesheet.rules; i++) {
+  if (value !== "") {
+    let query = value.trim().split(" ");
+    let mapper = x => `.card-grid > .card:not([data-match*="${x}"])`;
+    let attrSelector = query.map(mapper).join(", ");
+    rule = `${attrSelector} { display: none; }`;
+  }
+
+  for (let i = 0; i < stylesheet.cssRules.length; i++) {
     stylesheet.deleteRule(0);
   }
 
-  if (query !== "") {
-    stylesheet.insertRule(rule, 1);
+  if (rule !== null) {
+    stylesheet.insertRule(rule, 0);
   }
 }
