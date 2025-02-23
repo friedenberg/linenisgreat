89c50d529f2c913764f0b2f3aa0248a4339a1ef6
Sasha F <cornflake.matchbox397@icantbelieveitsnotgmail.com>
2025-02-23 10:13:31 -0500

    update

---
+++ protected/lib/Route.php
@@ -0,0 +1,162 @@
+<?php declare(strict_types=1);
+
+class Route {
+  public $nav;
+  public $mustache;
+  public $title;
+
+  /**
+   * @param string $title
+   */
+  function __construct(
+    $title,
+  ) {
+    $this->nav = new Nav($title);
+    $this->title = $title;
+
+    $options = array('extension' => '.html.mustache');
+
+    $this->mustache = new Mustache_Engine(array(
+      'loader' => new Mustache_Loader_FilesystemLoader(
+        __DIR__ . '/templates',
+        $options,
+      ),
+      'entity_flags' => ENT_QUOTES,
+    ));
+  }
+
+  /**
+   * @return array<string,string>
+   */
+  function getSiteData() : array {
+    return [
+      "title" => $this->title,
+      "favicon" => "assets/favicon.png",
+    ];
+
+  }
+  function getCodeMetaRaw() : string {
+    if (strcmp($this->title, "code") != 0) {
+      return "";
+    }
+
+    if (empty($this->path) || strcmp($this->path, "/") == 0) {
+      return "";
+    }
+
+    $elements = explode("/", $this->path);
+    $key = $elements[1];
+    $raw = $this->parser->getRaw();
+    $zettel = [];
+
+    foreach ($raw as $someZettel) {
+      if (strcmp($someZettel['akte']['kennung'], $key) === 0 ) {
+        $zettel = $someZettel;
+        break;
+      }
+    }
+
+    /* $zettel = $raw[$key] ?? []; */
+
+    if (empty($zettel)) {
+      // TODO 404
+    }
+
+    $code = $zettel['akte']['meta'];
+    /* $remainder = implode("/", array_slice($elements, 2)); */
+    /* $code['name'] = $code['name'] . '/' . $remainder; */
+    /* $code['url'] = $code['url'] . $remainder; */
+
+    return $this->mustache->render($code['template'], $code);
+  }
+
+  /**
+   * @return array<string,string>
+   */
+  function getMeta() : array {
+    $meta = $this->getSiteData();
+    $meta['raw'] = $this->getCodeMetaRaw($this->mustache);
+
+    return $meta;
+  }
+
+  function getCardTemplate() : string{
+    switch ($this->title) {
+    case "code":
+      return "card_code_project";
+
+    default:
+      return "cocktail_card";
+    }
+  }
+
+  function getObjects($objectClassName, $urlPrefix) : array {
+    if (isset($this->objects)) {
+      return $this->objects;
+    }
+
+    $this->objects = $this->parser->parseCustomClass($objectClassName, $urlPrefix);
+
+    /* foreach ($this->objects as $object) { */
+    /*   $path = $object->getLocalPath($this->mustache); */
+
+    /*   if (file_exists($path)) { */
+    /*     continue; */
+    /*   } */
+
+    /*   $object->writeToPath($path); */
+    /* } */
+
+    $this->objects = array_values($this->objects);
+
+    return $this->objects;
+  }
+
+  private function makeTemplateArgs(...$extra) : array {
+    return array_merge(
+      [
+        'nav' => array_values($this->nav->tiles),
+        'meta' => $this->getMeta(),
+        'stylesheets' => [
+          "stylesheet",
+          "zettels",
+          "fonts",
+        ],
+      ],
+      ...$extra,
+    );
+  }
+
+  function render(
+    $template,
+    $args = [],
+  ) : void {
+    $mustache = $this->mustache;
+
+    echo $this->mustache->render(
+      $template,
+      $this->makeTemplateArgs(
+        $args,
+      ),
+    );
+  }
+
+  function renderWithExtraStylesheets(
+    $template,
+    $stylesheets,
+    $args,
+  ) : void {
+    $mustache = $this->mustache;
+
+    $args = $this->makeTemplateArgs(
+      $args,
+    );
+
+    array_push($args["stylesheets"], ...$stylesheets);
+
+    echo $this->mustache->render(
+      $template,
+      $args,
+    );
+  }
+}
--- public/resume.php
+++ public/resume.php
@@ -1,28 +1,15 @@
 <?php declare(strict_types=1);
 
-$options =  array('extension' => '.html.mustache');
+$tab = new Route('resume');
 
-$mustache = new Mustache_Engine(array(
-  'loader' => new Mustache_Loader_FilesystemLoader(dirname(__FILE__) . '/../protected/lib/templates', $options),
-  'entity_flags' => ENT_QUOTES,
-));
-
-$zettels = new Tab('resume');
-$nav = new Nav("resume");
-
-$template = 'resume';
-$template_args = [
-  'nav' => array_values($nav->tiles),
-  'meta' => $zettels->getMeta(),
-  'stylesheets' => [
-    "stylesheet",
-    "zettels",
-    "fonts",
+$tab->renderWithExtraStylesheets(
+  'resume',
+  [
+    'resume',
   ],
-  "resume" => file_get_contents(
-    __DIR__ . "/resume.html",
-  ),
-];
-
-$template_args['query'] = substr($_SERVER['REQUEST_URI'], 1);
-echo $mustache->render($template, $template_args);
+  [
+    "resume" => file_get_contents(
+      __DIR__ . "/resume.html",
+    ),
+  ],
+);
